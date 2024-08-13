1763351c4e86f592337bf4b92d2bdc1a0f3a7ab0
Usman Abbasi <93172268+UsmanAbbasii@users.noreply.github.com>
2024-08-13 11:01:30 -0700

    deleted app 1

--- app (1).py
+++ app (1).py
@@ -1,16 +1,1 @@
-import streamlit as st
-from Home import home_page
-from DataExtraction import data_extraction_page
-from face_comparison import face_comparison_page
 
-PAGES = {
-    "Home": home_page,
-    "Data Extraction": data_extraction_page,
-    "Face Comparison": face_comparison_page
-}
-
-st.sidebar.title('Navigation')
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-page = PAGES[selection]
-page()
